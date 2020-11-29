ae9adeb98cac7861758c4005e46986e7149613e0
unknown <s.marek.adamczyk@gmail.com>
2020-11-29 17:55:59 +0100

    commit before firebase

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Header from "../Header/Header";
 
-const Main = () => {
+const Main = (user) => {
     return (
         <section className={"main-container"}>
             <Header/>
-            <h1 className={"main-title"}> Welcome in <span>HandiCraftin Market</span></h1>
+            <h1 className={"main-title"}> Welcome {user.user} in <span>HandiCraftin Market</span></h1>
         </section>
     )
 }
