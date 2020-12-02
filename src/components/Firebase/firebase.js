cd8b5a9b0e73b11a063c12d88b36d664b71e0890
unknown <s.marek.adamczyk@gmail.com>
2020-12-02 23:57:34 +0100

    environment variables

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,21 +1,23 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
+
 
 const config = {
-    apiKey: "AIzaSyCAAqqzYBaZMj0ZX3Jqj7Ej16RgnV2rRuk",
-    authDomain: "handicraftin-market.firebaseapp.com",
-    databaseURL: "https://handicraftin-market.firebaseio.com",
-    projectId: "handicraftin-market",
-    storageBucket: "handicraftin-market.appspot.com",
-    messagingSenderId: "574769306607",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 class Firebase{
     constructor() {
         app.initializeApp(config);
 
-
         this.auth = app.auth();
+        this.db = app.database();
     }
 
     doCreateUserWithEmailAndPassword = (email, password) =>
@@ -30,6 +32,11 @@ class Firebase{
         this.auth.currentUser.updatePassword(password);
 
     doSignOut = () => this.auth.signOut();
+
+
+    user = uid => this.db.ref(`users/${uid}`);
+
+    users = () => this.db.ref('users');
 }
 
 export default Firebase;
