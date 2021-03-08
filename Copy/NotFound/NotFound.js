692657aba25dcaa44fdbbe14859749c4bd49ff64
Marek Adamczyk <s.marek.adamczyk@gmail.com>
2021-03-08 21:10:18 +0100

    delete old files

--- Copy/NotFound/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const NotFound = () => {
-    return (
-        <h3 className={"not-found-text"}>Whoops, wrong page i supose <i className="far fa-sad-cry"></i></h3>
-    )
-}
-
-export default NotFound;
